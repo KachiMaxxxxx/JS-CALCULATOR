64a9a9aac033c76751e4bb09582ff6e8473b8617
KachiMaxxxxx <kachimax0101@gmail.com>
2025-05-15 22:39:00 +0100

    optimised few js code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", () => {
-   
+
     const screen = document.querySelector(".screen");
     const buttons = document.querySelectorAll(".btn:not(.equal, .AC, .DE)");
     const clear = document.querySelector(".btn.AC.operator");
@@ -7,31 +7,28 @@ addEventListener("DOMContentLoaded", () => {
     const deleteNum = document.querySelector(".btn.DE.operator");
 
     buttons.forEach((button) => {
-       button.addEventListener("click", (e) => {
-        const value = e.target.dataset.num;
-        screen.value += value;
-       });
+        button.addEventListener("click", (e) => {
+            const value = e.target.dataset.num;
+            screen.value += value;
+        });
 
     });
 
-     equals.addEventListener("click", (e) => {
-        if (screen.value === "") {
-            screen.value = "";
-        } else {
-            const answer = eval(screen.value); 
+    equals.addEventListener("click", (e) => {
+        if (screen.value !== "") {
+            const answer = eval(screen.value);
             screen.value = answer;
-    
         }
     });
 
     clear.addEventListener("click", (e) => {
         screen.value = "";
     });
-    
 
-     deleteNum.addEventListener("click", (e) => {
-            
-            screen.value = screen.value.slice(0, -1);
+
+    deleteNum.addEventListener("click", (e) => {
+
+        screen.value = screen.value.slice(0, -1);
     });
-    
+
 });
